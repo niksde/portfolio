6d90c289ea3e7d44fa5bf7fe05545101d96ba91b
Nikhil Shisode <nikhilshisodemi6@gmail.com>
2022-11-06 00:05:04 +0530

    Added dark theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,94 +15,82 @@ const Home: NextPage = () => {
           href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/279/man-technologist_1f468-200d-1f4bb.png"
         />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.heroSection}>
-          <b className={styles.description}>Hello 👋</b>
-          <h1 className={styles.title}>
-            I&apos;m Nikhil Shisode, a Fullstack Developer 👨‍💻
-          </h1>
-          <p className={styles.description}>
-            Software Engineering professional with a B.E focused in Computer
-            Science from Dr. D. Y. Patil Institute of Engineering & Technology,
-            Pune.
-          </p>
-        </div>
-        <div className={styles.grid}>
-          <Card
-            title="Development Ability"
-            list={["Frontend", "Backend API", "Cross Platform Mobile App"]}
-          />
-          <Card
-            title="Technologies"
-            list={[
-              "React",
-              "Python",
-              "Redux",
-              "React Native",
-              "Nodejs",
-              "Expressjs",
-              "Nestjs",
-            ]}
-          />
-          <Card
-            title="Databases"
-            list={["MongoDB", "DynamoDB", "PostgreSQL", "MySQL"]}
-          />
-          <div>
-            <h2>Experience</h2>
-            <div className={styles.grid}>
-              <ExperienceCard
-                job="Fullstack Developer"
-                organisation="Center for Computational Technologies"
-                positionList={[
-                  {
-                    position: "Senior Member Of Technical Staff",
-                    duration: "Nov 2021 - Present",
-                  },
-                  {
-                    position: "Member Of Technical Staff",
-                    duration: "Oct 2020 - Oct 2021",
-                    Description: [
-                      <p key="position-desc-2">
-                        Development, Deployment and Maintenance of
-                      </p>,
-                      <ul key="position-list-2">
-                        <li>
-                          Frontend web applications made with react, threejs.
-                        </li>
-                        <li>
-                          Backend API on AWS cloud made with python, docker, js,
-                          lambda, dynamodb, appsync.
-                        </li>
-                      </ul>,
-                    ],
-                  },
-                ]}
-              />
-              <ExperienceCard
-                job="(Freelance) Backend Developer"
-                organisation="CraftVeda Technology"
-                positionList={[
-                  {
-                    position: "Backend Developer",
-                    duration: "May 2020 - July 2020, Kolkata (remote)",
-                    Description: [
-                      <p key="position-desc-1">Design, Development of</p>,
-                      <ul key="position-list-1">
-                        <li>
-                          Backend API using nodejs, AWS, lambda, dynamodb, s3
-                          and serverless framework.
-                        </li>
-                      </ul>,
-                    ],
-                  },
-                ]}
-              />
+      <div className={styles.body}>
+        <main className={styles.main}>
+          <div className={styles.heroSection}>
+            <b className={styles.description}>Hello 👋</b>
+            <h1 className={styles.title}>
+              I&apos;m Nikhil Shisode, a
+              <span className={styles.accent}> Fullstack Developer</span> 👨‍💻
+            </h1>
+            <p className={styles.description}>
+              Software Engineering professional with a B.E focused in Computer
+              Science from Dr. D. Y. Patil Institute of Engineering &
+              Technology, Pune.
+            </p>
+          </div>
+          <div className={styles.grid}>
+            <Card
+              title="Development Ability"
+              list={["Frontend", "Backend API", "Cross Platform Mobile App"]}
+            />
+            <Card
+              title="Technologies"
+              list={[
+                "React",
+                "Python",
+                "Redux",
+                "React Native",
+                "Nodejs",
+                "Expressjs",
+                "Nestjs",
+              ]}
+            />
+            <Card
+              title="Databases"
+              list={["MongoDB", "DynamoDB", "PostgreSQL", "MySQL"]}
+            />
+            <div className={styles.mainExpSection}>
+              <h2>Experience</h2>
+              <div className={styles.grid}>
+                <ExperienceCard
+                  job="Fullstack Developer"
+                  organisation="Center for Computational Technologies"
+                  positionList={[
+                    {
+                      position: "Team Lead",
+                      duration: "Apr 2022 - Present",
+                    },
+                    {
+                      position: "Senior Member Of Technical Staff",
+                      duration: "Nov 2021 - Apr 2022",
+                    },
+                    {
+                      position: "Member Of Technical Staff",
+                      duration: "Oct 2020 - Oct 2021",
+                      Description: [
+                        <p key="position-desc-2">
+                          Development, Deployment and Maintenance of
+                        </p>,
+                        <ul key="position-list-2">
+                          <li>
+                            Frontend web applications made with react, threejs.
+                          </li>
+                          <li>
+                            Backend API on AWS cloud made with python, docker,
+                            js, lambda, dynamodb, appsync.
+                          </li>
+                        </ul>,
+                      ],
+                    },
+                  ]}
+                />
+              </div>
             </div>
           </div>
-        </div>
-      </main>
-      <footer className={styles.footer}>Nikhil Shisode</footer>
+        </main>
+        <footer className={styles.footer}>Nikhil Shisode</footer>
+      </div>
     </div>
   );
 };
